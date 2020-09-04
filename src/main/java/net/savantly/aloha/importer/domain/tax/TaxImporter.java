7f58b767af24d9b912a56992a5474f099e1c71cd
Jeremy Branham <Jeremy@savantly.net>
2020-09-04 00:18:47 -0500

    adds tax importer

--- src/main/java/net/savantly/aloha/importer/domain/tax/TaxImporter.java
+++ src/main/java/net/savantly/aloha/importer/domain/tax/TaxImporter.java
@@ -9,8 +9,8 @@ import net.savantly.aloha.importer.domain.importedFiles.ImportedFileRepository;
 @Service
 public class TaxImporter extends AbstractDbfImporter<Tax, TaxId> {
 
-	public TaxImporter(CrudRepository<Tax, TaxId> repo, ImportedFileRepository importedFiles, Class<Tax> clazz) {
-		super(repo, importedFiles, clazz);
+	public TaxImporter(CrudRepository<Tax, TaxId> repo, ImportedFileRepository importedFiles) {
+		super(repo, importedFiles, Tax.class);
 	}
 
 }
